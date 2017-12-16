9344d37001c8c51bc377e242cbaf9a0c472c5501
jinke.li <1359518268@qq.com>
2017-12-16 23:32:11 +0800

    feat

---
+++ 前端常用算法/bubbleSort.js
@@ -0,0 +1,17 @@
+function bubbleSort(arr){
+  var i = 0,
+      j = 0;
+  for(i=1; i<arr.length; i++){
+    for(j=0; j<=arr.length-i; j++){
+      var temp = 0;
+      // ">" 从小到大排序
+      // "<" 从大到小排序
+      if(arr[j] > arr[j+1]){
+        temp = arr[j];
+        arr[j] = arr[j+1];
+        arr[j+1] = temp;
+      }
+    }
+  }
+  return arr;
+}
