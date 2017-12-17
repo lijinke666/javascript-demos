41077ffc879b789668e19171f241887d22269da9
jinke.li <1359518268@qq.com>
2017-12-17 20:32:17 +0800

    add

---
+++ 前端常用算法/merge.js
@@ -0,0 +1,25 @@
+/**
+ * @name 二路归并
+ * @param {*} left 
+ * @param {*} right 
+ */
+function merge(left, right) {
+    var result = [],
+        il = 0,
+        ir = 0;
+
+    while (il < left.length && ir < right.length) {
+        if (left[il] < right[ir]) {
+            result.push(left[il++]);
+        } else {
+            result.push(right[ir++]);
+        }
+    }
+    while(left[il]){
+        result.push(left[il++]);
+    }
+    while(right[ir]){
+        result.push(right[ir++]);
+    }
+    return result;
+}
