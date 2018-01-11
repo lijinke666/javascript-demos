b42f7343fceaa33eecd338a08a75023ec3bca5be
jinke <1359518268@qq.com>
2018-01-11 14:20:43 +0800

    feat:冒泡排序

--- 前端常用算法/bubbleSort.js
+++ 前端常用算法/bubbleSort.js
@@ -2,18 +2,20 @@
  * @name 冒泡排序
  * @param {*} arr 
  */
-function bubbleSort(arr){
+function bubbleSort(arr) {
   var i = 0,
-      j = 0;
-  for(i=1; i<arr.length; i++){
-    for(j=0; j<=arr.length-i; j++){
-      var temp = 0;
+    j = 0,
+    temp = 0
+  for (i = 1; i < arr.length; i++) {
+    for (j = 0; j <= arr.length - i; j++) {
       // ">" 从小到大排序
       // "<" 从大到小排序
-      if(arr[j] > arr[j+1]){
+      //比较右边的数  如果大于本身 就交换位置
+      if (arr[j] > arr[j + 1]) {
+        //用temp 来保存 之前的数
         temp = arr[j];
-        arr[j] = arr[j+1];
-        arr[j+1] = temp;
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
       }
     }
   }
@@ -21,4 +23,26 @@ function bubbleSort(arr){
 }
 
 
-console.log(bubbleSort([1,2,4,7,3]));
+console.log(bubbleSort([1, 2, 4, 7, 3]));
+
+/**
+ * 
+ * @param {*} arr 数组 
+ * @param {*} desc true 降序 false 升序
+ */
+const mySort = (arr, desc = false) => {
+  let [i,j,temp] = [0,0,0]
+  let len = arr.length
+  for(i =1; i< len; i++){
+    for(j =0; j<= len -i ; j++){
+      if( desc ? arr[j] < arr[j+1] : arr[j] > arr[j+1]){
+        temp = arr[j]
+        arr[j] = arr[j+1]
+        arr[j+1] = temp
+      }
+    }
+  }
+  return arr
+}
+
+console.log(mySort([2,5,1,3,4],true));
