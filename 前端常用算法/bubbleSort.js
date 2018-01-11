d0680889fbc4cd8ea72e6086a39f1ddbcdca07ab
jinke <1359518268@qq.com>
2018-01-11 15:29:10 +0800

    feat:冒泡排序 函数式 实现

--- 前端常用算法/bubbleSort.js
+++ 前端常用算法/bubbleSort.js
@@ -31,18 +31,44 @@ console.log(bubbleSort([1, 2, 4, 7, 3]));
  * @param {*} desc true 降序 false 升序
  */
 const mySort = (arr, desc = false) => {
-  let [i,j,temp] = [0,0,0]
+  let [i, j, temp] = [0, 0, 0]
   let len = arr.length
-  for(i =1; i< len; i++){
-    for(j =0; j<= len -i ; j++){
-      if( desc ? arr[j] < arr[j+1] : arr[j] > arr[j+1]){
+  for (i = 1; i < len; i++) {
+    for (j = 0; j <= len - i; j++) {
+      if (desc ? arr[j] < arr[j + 1] : arr[j] > arr[j + 1]) {
         temp = arr[j]
-        arr[j] = arr[j+1]
-        arr[j+1] = temp
+        arr[j] = arr[j + 1]
+        arr[j + 1] = temp
       }
     }
   }
   return arr
 }
 
-console.log(mySort([2,5,1,3,4],true));
+// console.log(mySort([2, 5, 1, 3, 4], true));  //[ 5, 4, 3, 2, 1 ]
+// console.log(mySort(["2", "3", "1", "9", 4], false));  //[ '1', '2', '3', 4, '9' ]
+
+/**
+ * 函数式编程的排序
+ * 实现 Array.prototype.sort 一样的效果
+ * @param {*} arr 
+ */
+const sort = (arr, fn) => {
+  let [i, j, temp] = [0, 0, 0]
+  const len = arr.length
+  const tempArr = [...arr] //用来保存 函数参数
+  for (i = 1; i < len; i++) {
+    const DESC = fn(tempArr[i - 1], tempArr[i])
+    for (j = 0; j <= len - i; j++) {
+      if (DESC ? arr[j] < arr[j + 1] : arr[j] > arr[j + 1]) {
+        temp = arr[j]
+        arr[j] = arr[j + 1]
+        arr[j + 1] = temp
+      }
+    }
+  }
+  return arr
+}
+
+const result = sort([1, 3, 2, 5, 4], (a, b) => a > b)
+console.log(result);
