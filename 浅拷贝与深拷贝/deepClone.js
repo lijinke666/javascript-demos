bbb8a4416d8630bc90eeadb137bffa348f008b0a
Jinke.Li <1359518268@qq.com>
2018-07-12 17:20:05 +0800

    feat:add

--- 浅拷贝与深拷贝/deepClone.js
+++ 浅拷贝与深拷贝/deepClone.js
@@ -16,3 +16,20 @@ const testObj = {
 copyObj.info[0] = "码农"
 console.log(copyObj.info);     //[ '码农', '码畜' ]
 console.log(testObj.info);     //[ '屌丝', '码畜' ]
+
+
+const deepClone = (obj)=>{
+  const o = Array.isArray(obj) ? [] : {}
+  for(let i in obj){
+    const current = obj[i]
+    //如果value 是对象 就递归
+    o[i] =  typeof(current) === 'object' ?  deepClone(current) : obj[i]
+  }
+  return o
+}
+
+console.log('==========')
+const copyObj2 = deepClone(testObj)
+copyObj2.info[0] = "码农333"
+console.log(copyObj2.info);     //[ '码农333', '码畜' ]
+console.log(testObj.info);     //[ '屌丝', '码畜' ]
