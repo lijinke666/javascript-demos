5e69eb1ddfea9ae8b142c5e0ae6df4a6dbc8075b
Jinke.Li <1359518268@qq.com>
2018-07-26 11:19:17 +0800

    feat:add

---
+++ 正则捕获组/index.js
@@ -0,0 +1,15 @@
+const url = 'test.jpg'
+
+//传统方式 
+const name = url.replace(/(.*)\..*/,'$1')
+console.log(name);  //test
+
+const ext = url.replace(/.*\.(.*)/,'$1')
+console.log(ext)    //jpg
+
+
+//更直观的  es1028
+
+const {groups:{name,ext}} = /(?<name>.*)\.(?<ext>.*)/.exec(url)
+
+console.log(name,ext)   // test,jpg
