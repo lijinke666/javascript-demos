8257d615c436f267b4e6be359fa9e3c3e9c21c54
Jinke.Li <1359518268@qq.com>
2018-12-03 14:46:21 +0800

    feat: iterator

---
+++ iterator.js
@@ -0,0 +1,12 @@
+var arr = ['w', 'y', 'k', 'o', 'p'];
+var eArr = arr[Symbol.iterator]();
+console.log(eArr.next().value); // w
+console.log(eArr.next().value); // y
+console.log(eArr.next().value); // k
+console.log(eArr.next().value); // o
+console.log(eArr.next().value);
+
+
+for (let letter of eArr) {
+  console.log(letter.next().value);
+}
