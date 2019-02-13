705a8aa0ede99bffd25e74e2c12e031837544aa1
Jinke Li <1359518268@qq.com>
2019-02-13 15:21:51 +0800

    对象数组 的差集

--- 数组的交集和差集.js
+++ 数组的交集和差集.js
@@ -14,3 +14,15 @@ console.log(arr1.filter((v) => !new Set(arr2).has(v)));
 //输出：[1, 2]
 console.log(arr2.filter((v) => !new Set(arr1).has(v)));
 //输出：[4, 5]
+
+
+//object[] 的 差集
+const arr1 = [{a:1},{a:2}]
+const arr2 = [{a:1},{a:2},{a:3}]
+const result = arr2.filter(
+    (audio, i) =>
+      arr1.findIndex(
+        v => v.a === audio.a
+      ) === -1
+  )
+// 输出 : [{a:3}]
