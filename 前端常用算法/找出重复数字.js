33d243fb728f0af3bdaf167d41ab469422ad50a7
Jinke.Li <1359518268@qq.com>
2019-05-31 11:45:03 +0800

    feat(算法): 通过字典 优化  找出重复数字 算法 O(n)

--- 前端常用算法/找出重复数字.js
+++ 前端常用算法/找出重复数字.js
@@ -3,7 +3,7 @@
  * @param  {...number} num
  */
 
-// 双重循环 暴力版  时间复杂度 O(n^2)
+// 双重循环 暴力版  时间复杂度 O(n^2)  耗时: 0.1s
 const repeat = (...num) => {
   const validatedNum = []
   const repeatNum = []
@@ -17,5 +17,25 @@ const repeat = (...num) => {
   return repeatNum
 }
 
-console.log(repeat(3, 1, 2, 5, 4, 9, 7, 2))   // [2]
-console.log(repeat(3, 1, 2, 5, 4, 9, 7, 2, 1)) // [2,1]
+// console.log(repeat(3, 1, 2, 5, 4, 9, 7, 2))   // [2]
+// console.log(repeat(3, 1, 2, 5, 4, 9, 7, 2, 1)) // [2,1]
+
+
+// 通过字典来 优化 时间复杂度 O(n)   耗时: 0.096s
+const repeatByDict = (...num) => {
+  // 这里用字典来存储 => O(1)
+  const validatedNum = new Map()
+
+  const repeatNum = []
+  for (let i = 0; i <= num.length; i++) {
+    if(!validatedNum.has(num[i])) {
+      validatedNum.set(num[i], true)
+    } else {
+      repeatNum.push(num[i])
+    }
+  }
+  return repeatNum
+}
+
+console.log(repeatByDict(3, 1, 2, 5, 4, 9, 7, 2))   // [2]
+console.log(repeatByDict(3, 1, 2, 5, 4, 9, 7, 2, 1)) // [2,1]
