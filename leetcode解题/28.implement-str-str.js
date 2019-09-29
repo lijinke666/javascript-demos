2727bb4aa2562f01cbdbcb67cc28a633e83c29cd
Jinke.Li <1359518268@qq.com>
2019-09-29 15:20:22 +0800

    feat(leetCode): [28] Implement strStr()

---
+++ leetcode解题/28.implement-str-str.js
@@ -0,0 +1,51 @@
+/*
+ * @lc app=leetcode id=28 lang=javascript
+ *
+ * [28] Implement strStr()
+ *
+ * https://leetcode.com/problems/implement-strstr/description/
+ *
+ * algorithms
+ * Easy (32.88%)
+ * Likes:    1053
+ * Dislikes: 1502
+ * Total Accepted:    497.9K
+ * Total Submissions: 1.5M
+ * Testcase Example:  '"hello"\n"ll"'
+ *
+ * Implement strStr().
+ *
+ * Return the index of the first occurrence of needle in haystack, or -1 if
+ * needle is not part of haystack.
+ *
+ * Example 1:
+ *
+ *
+ * Input: haystack = "hello", needle = "ll"
+ * Output: 2
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: haystack = "aaaaa", needle = "bba"
+ * Output: -1
+ *
+ *
+ * Clarification:
+ *
+ * What should we return when needle is an empty string? This is a great
+ * question to ask during an interview.
+ *
+ * For the purpose of this problem, we will return 0 when needle is an empty
+ * string. This is consistent to C's strstr() and Java's indexOf().
+ *
+ */
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function(haystack, needle) {
+  return haystack.indexOf(needle)
+}
