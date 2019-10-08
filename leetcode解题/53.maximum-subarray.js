b391669d8b837ee4e98b56a09df368e5b60ae00f
Jinke.Li <1359518268@qq.com>
2019-10-08 22:35:01 +0800

    feat(leetCode): [53] Maximum Subarray

--- leetcode解题/53.maximum-subarray.js
+++ leetcode解题/53.maximum-subarray.js
@@ -36,53 +36,23 @@
  * @param {number[]} nums
  * @return {number}
  */
-// 暂时想到的思路 找到首尾都是 正整数的数组 比较最大的一组?
-// var maxSubArray = function(nums) {
-//   let a, b
-//   const sum = (nums) => nums.reduce((num, v) => (num += v), 0)
-//   const group = []
-//   const reverseNums = [...nums].reverse()
-//   for (let i = 0; i < nums.length; i++) {
-//     const firstIndex = nums.findIndex((n) => n >= 0 && n !== a)
-//     const lastIndex = reverseNums.findIndex((n) => n >= 0 && b !== n)
-//     console.log('firstIndex: ', firstIndex, lastIndex)
-//     const _lastIndex = nums.length - lastIndex
-//     console.log(a, b)
-//     if (firstIndex === -1 && lastIndex === -1) {
-//       group.push([Math.max(...nums)])
-//     } else {
-//       a = nums[firstIndex]
-//       b = nums[_lastIndex - 1]
-//       group.push(nums.slice(firstIndex, _lastIndex))
-//     }
-//   }
-//   console.log('group: ', group)
-//   return Math.max(...group.map(sum))
-// }
-
+// 卡在 第 200 关 内存不够
 var maxSubArray = function(nums) {
   const sum = (nums) => nums.reduce((num, v) => (num += v), 0)
   const group = []
   for (let i = 0; i < nums.length; i++) {
-    if (nums[i] < 0) {
-      continue
-    }
-    for (let j = i; j < nums.length; j++) {
-      if (nums[j] >= 0) {
-        group.push(nums.slice(i, j + 1))
+    if (nums[i] >= 0) {
+      for (let j = i; j < nums.length; j++) {
+        if (nums[j] >= 0) {
+          group.push(sum(nums.slice(i, j + 1)))
+        }
       }
     }
   }
-  const result = Math.max(...group.map(sum))
-  if (result === -Infinity) {
+  if (!group.length) {
     return Math.max(...nums)
   }
-  return result
+  return Math.max(...group)
 }
-
-console.log(maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4]))
-console.log(maxSubArray([-1, -2]))
-console.log(maxSubArray([8, -19, 5, -4, 20]))
-console.log(maxSubArray([1, 1, -2]))
 
 // @lc code=end
