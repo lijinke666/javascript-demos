90106671be46e353fecebe4c9e8fb61189086cb9
Jinke.Li <1359518268@qq.com>
2019-10-09 09:43:10 +0800

    feat(leetCode): [20] Valid Parentheses 使用栈数据结构重构

--- leetcode解题/20.valid-parentheses.js
+++ leetcode解题/20.valid-parentheses.js
@@ -61,65 +61,92 @@
  *
  *
  */
+// /**
+//  * @param {string} s
+//  * @return {boolean}
+//  */
+
+// const map = {
+//   '(': ')',
+//   ')': '(',
+//   '{': '}',
+//   '}': '{',
+//   '[': ']',
+//   ']': '['
+// }
+// var isValid = function(s) {
+//   let result = false
+//   if (!s) {
+//     return true
+//   }
+//   if (s.length % 2 !== 0) {
+//     return false
+//   }
+//   const strings = s.split('')
+//   const group = strings.reduce((obj, v) => {
+//     if (!obj[v]) {
+//       obj[v] = 1
+//     } else {
+//       obj[v]++
+//     }
+//     return obj
+//   }, {})
+
+//   result = Object.keys(group).every((key) => {
+//     const open = group[key]
+//     const close = group[map[key]]
+//     return open === close
+//   })
+
+//   if (result) {
+//     for (let index = 0; index < strings.length / 2; index++) {
+//       const element = strings[index]
+//       if (
+//         element === map[strings[strings.length - index - 1]] ||
+//         (strings[index * 2] &&
+//           strings[index * 2 + 1] &&
+//           strings[index * 2] === map[strings[index * 2 + 1]])
+//       ) {
+//         result = true
+//       } else {
+//         console.log(1)
+//         result = false
+//       }
+//     }
+//   }
+
+//   return result
+// }
+
 /**
- * @param {string} s
- * @return {boolean}
+ * @description FIXME: 这道题没做出来
+ * 正确解法
+ * 使用 栈  push 入栈 pop 出栈
+ * 1. 遇到左括号 入栈 遇到 右括号出栈
+ * 2. 最后 栈为空 则是一组无效的括号
  */
-
-const map = {
-  '(': ')',
-  ')': '(',
-  '{': '}',
-  '}': '{',
-  '[': ']',
-  ']': '['
-}
 var isValid = function(s) {
-  let result = false
-  if (!s) {
-    return true
+  const map = {
+    '{': '}',
+    '[': ']',
+    '(': ')'
   }
-  if (s.length % 2 !== 0) {
-    return false
-  }
-  const strings = s.split('')
-  const group = strings.reduce((obj, v) => {
-    if (!obj[v]) {
-      obj[v] = 1
+  const stack = []
+  for (let i = 0; i < s.length; i++) {
+    const current = s[i]
+    if (Object.keys(map).includes(current)) {
+      stack.push(current)
     } else {
-      obj[v]++
-    }
-    return obj
-  }, {})
-
-  result = Object.keys(group).every((key) => {
-    const open = group[key]
-    const close = group[map[key]]
-    return open === close
-  })
-
-  if (result) {
-    for (let index = 0; index < strings.length / 2; index++) {
-      const element = strings[index]
-      if (
-        element === map[strings[strings.length - index - 1]] ||
-        (strings[index * 2] &&
-          strings[index * 2 + 1] &&
-          strings[index * 2] === map[strings[index * 2 + 1]])
-      ) {
-        result = true
-      } else {
-        console.log(1)
-        result = false
+      // 此时当前栈 里面都是 左括号 进 else 的都是 右括号
+      // 用左括号 map 对应的 隐射 与 当前 括号比较 如果不相等 则无效
+      const last = stack.pop()
+      if (map[last] !== current) {
+        return false
       }
     }
   }
-
-  return result
+  if (stack.length) {
+    return false
+  }
+  return true
 }
-
-// console.log(isValid('()'))
-// console.log(isValid('[(({})}]'))
-// console.log(isValid('([)]'))
-// console.log(isValid('()[]{}'))
-console.log(isValid('(([]){})'))
