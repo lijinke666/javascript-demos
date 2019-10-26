eb96a61be1b665124f8a3371b226c8a2f01ac4ca
Jinke.Li <1359518268@qq.com>
2019-10-26 21:50:37 +0800

    feat(leetCode):[88] Merge Sorted Array

--- leetcode解题/88.merge-sorted-array.js
+++ leetcode解题/88.merge-sorted-array.js
@@ -15,25 +15,25 @@
  *
  * Given two sorted integer arrays nums1 and nums2, merge nums2 into nums1 as
  * one sorted array.
- * 
+ *
  * Note:
- * 
- * 
+ *
+ *
  * The number of elements initialized in nums1 and nums2 are m and n
  * respectively.
  * You may assume that nums1 has enough space (size that is greater or equal to
  * m + n) to hold additional elements from nums2.
- * 
- * 
+ *
+ *
  * Example:
- * 
- * 
+ *
+ *
  * Input:
  * nums1 = [1,2,3,0,0,0], m = 3
  * nums2 = [2,5,6],       n = 3
- * 
+ *
  * Output: [1,2,2,3,5,6]
- * 
+ *
  */
 
 // @lc code=start
@@ -45,7 +45,9 @@
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
 var merge = function(nums1, m, nums2, n) {
-    
-};
+  // 暴力解法 由于是 有序数组 删除 0 的下标 合并起来 拍一次序
+  nums1.splice(m, nums1.length - m)
+  nums2.splice(n, nums2.length - n)
+  Object.assign(nums1, [...nums1, ...nums2].sort((a, b) => a - b))
+}
 // @lc code=end
-
