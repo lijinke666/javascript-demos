93562a351789024f9b8aee2fcc1fd399d51f1f35
Jinke.Li <1359518268@qq.com>
2019-11-04 00:33:43 +0800

    feat(leetCode): [107] Binary Tree Level Order Traversal II 二叉树的层次遍历

--- leetcode解题/107.binary-tree-level-order-traversal-ii.js
+++ leetcode解题/107.binary-tree-level-order-traversal-ii.js
@@ -15,28 +15,28 @@
  *
  * Given a binary tree, return the bottom-up level order traversal of its
  * nodes' values. (ie, from left to right, level by level from leaf to root).
- * 
- * 
+ *
+ *
  * For example:
  * Given binary tree [3,9,20,null,null,15,7],
- * 
+ *
  * ⁠   3
  * ⁠  / \
  * ⁠ 9  20
  * ⁠   /  \
  * ⁠  15   7
- * 
- * 
- * 
+ *
+ *
+ *
  * return its bottom-up level order traversal as:
- * 
+ *
  * [
  * ⁠ [15,7],
  * ⁠ [9,20],
  * ⁠ [3]
  * ]
- * 
- * 
+ *
+ *
  */
 
 // @lc code=start
@@ -52,7 +52,20 @@
  * @return {number[][]}
  */
 var levelOrderBottom = function(root) {
-    
-};
+  const result = []
+  // 在递归的同时, 用一个指针 表示当前的位置
+  const tree2Array = (node, index) => {
+    if (node) {
+      if (!result[index]) {
+        result[index] = []
+      }
+      result[index].push(node.val)
+      tree2Array(node.left, index + 1)
+      tree2Array(node.right, index + 1)
+    }
+  }
+  tree2Array(root, 0)
+  return result.reverse()
+}
+// console.log([[15, 7], [9, 20], [3]].reverse())
 // @lc code=end
-
