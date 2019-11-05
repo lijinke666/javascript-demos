b15a6e679353d182e5f296e589695461b285617e
Jinke.Li <1359518268@qq.com>
2019-11-05 18:51:09 +0800

    feat(leetCode): [108] Convert Sorted Array to Binary Search Tree

--- leetcode解题/108.convert-sorted-array-to-binary-search-tree.js
+++ leetcode解题/108.convert-sorted-array-to-binary-search-tree.js
@@ -15,26 +15,26 @@
  *
  * Given an array where elements are sorted in ascending order, convert it to a
  * height balanced BST.
- * 
+ *
  * For this problem, a height-balanced binary tree is defined as a binary tree
  * in which the depth of the two subtrees of every node never differ by more
  * than 1.
- * 
+ *
  * Example:
- * 
- * 
+ *
+ *
  * Given the sorted array: [-10,-3,0,5,9],
- * 
+ *
  * One possible answer is: [0,-3,9,-10,null,5], which represents the following
  * height balanced BST:
- * 
+ *
  * ⁠     0
  * ⁠    / \
  * ⁠  -3   9
  * ⁠  /   /
  * ⁠-10  5
- * 
- * 
+ *
+ *
  */
 
 // @lc code=start
@@ -50,7 +50,14 @@
  * @return {TreeNode}
  */
 var sortedArrayToBST = function(nums) {
-    
-};
+  if (!nums.length) {
+    return null
+  }
+  const root = new TreeNode(null)
+  // 2 分法  分成 左 中 右 三部分 一次递归
+  root.left = sortedArrayToBST(nums.splice(0, nums.length / 2))
+  root.val = nums[0]
+  root.right = sortedArrayToBST(nums.splice(1))
+  return root
+}
 // @lc code=end
-
