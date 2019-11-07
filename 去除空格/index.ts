e9d6b1e7642ea65afa24dadfa61b6b6f50490a09
Jinke.Li <1359518268@qq.com>
2019-11-07 23:43:14 +0800

    feat: 去除空格

---
+++ 去除空格/index.ts
@@ -0,0 +1,9 @@
+export const trim = (target, fullTrim = false) => {
+  if (!fullTrim) {
+    return target.trim()
+  }
+  return target.replace(/\s/gm, '')
+}
+
+console.log(trim('  trim  '));
+console.log(trim('  tri  m  ',true));
