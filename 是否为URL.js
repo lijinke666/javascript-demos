98456c89c67435cfdbf60a49081d47048cdad36a
lijinke666 <a1231236677287@163.com>
2020-02-27 18:41:59 +0800

    支持 localhost

--- 是否为URL.js
+++ 是否为URL.js
@@ -1,13 +1,15 @@
 function isURL(value) {
-  return /^(((ht|f)tps?):\/\/)?[\w-]+(\.[\w-]+)+([\w.,@?^=%&:\/~+#-]*[\w@?^=%&\/~+#-])?$/.test(
+  return /^((https?):\/\/)?(\w+|-)+(\.[\w-]+)?([\w.,@?^=%&:\/~+#-]*[\w@?^=%&\/~+#-])?$/.test(
     value
   )
 }
 
 console.log(isURL('http://www.baidu.com'))
+console.log(isURL('http://www.baidu.com:80'))
 console.log(isURL('http://www.baidu.com/v1'))
 console.log(isURL('http://www.baidu.com/v1.')) // false
 console.log(isURL('http://www.c-c-1.com'))
 console.log(isURL('https://www.c-c-1.com'))
 console.log(isURL('www.baidu.com'))
 console.log(isURL('www.baidu.com/v1'))
+console.log(isURL('http://localhost'))
