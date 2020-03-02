c526ddf7a6d4b85d2cf25fcccf77c9f9090bcf4e
lijinke666 <a1231236677287@163.com>
2020-03-02 18:20:17 +0800

    url

--- 是否为URL.js
+++ 是否为URL.js
@@ -1,5 +1,5 @@
 function isURL(value) {
-  return /^((https?):\/\/)?(\w+|-)+(\.[\w-]+)?([\w.,@?^=%&:\/~+#-]*[\w@?^=%&\/~+#-])?$/.test(
+  return /^((https?):\/\/)[\w-]*(\.[\w-]+|localhost)+([\w.,@?^=%&:\/~+#-]*[\w@?^=%&\/~+#-])?$/.test(
     value
   )
 }
@@ -12,6 +12,14 @@ console.log(isURL('http://www.c-c-1.com'))
 console.log(isURL('https://www.c-c-1.com'))
 console.log(isURL('www.baidu.com'))
 console.log(isURL('www.baidu.com/v1'))
+console.log('===')
 console.log(isURL('http://localhost'))
+console.log(isURL('http://localhost:80'))
 console.log(isURL('http://localhost/v1'))
 console.log(isURL('http://localhost.'))
+console.log(isURL('http'))
+console.log(isURL('https'))
+console.log(isURL('http://'))
+console.log(isURL('http://www'))
+console.log(isURL('11'))
+console.log(isURL(1))
