6507f47b9a3f81103102b9d7e40a98893540cc0a
jinke.li <1359518268@qq.com>
2020-03-24 21:36:25 +0800

    feat: 迷你redux

---
+++ 迷你redux/store.js
@@ -0,0 +1,50 @@
+/**
+ * @name 生成 store
+ * @param {*} reducer
+ * @param {*} initialState
+ */
+const createStore = (reducer, initialState) => {
+  const store = {
+    state: initialState, // 全局的 state
+    listeners: [] // 监听器 state 更新后触发
+  };
+
+  // 调用 subscribe 本质上就是 就是更新 store listeners 的数组
+  store.subscribe = listener => {
+    store.listeners.push(listener);
+  };
+
+  store.dispatch = action => {
+    // 通过reducer 更新 state
+    store.state = reducer(store.state, action);
+    store.listeners.forEach(listener => listener());
+  };
+
+  store.getState = () => store.state;
+
+  return store;
+};
+
+const COUNTER_ACTIONS = {
+  INCREMENT: "INCREMENT",
+  DECREMENT: "DECREMENT"
+};
+
+function counter(state = 0, action) {
+  switch (action.type) {
+    case COUNTER_ACTIONS.INCREMENT:
+      return state + 1;
+    case COUNTER_ACTIONS.DECREMENT:
+      return state - 1;
+    default:
+      return state;
+  }
+}
+
+const store = createStore(counter);
+
+store.subscribe(() => console.log(store.getState()));
+
+store.dispatch({ type: COUNTER_ACTIONS.INCREMENT });
+store.dispatch({ type: COUNTER_ACTIONS.INCREMENT });
+store.dispatch({ type: COUNTER_ACTIONS.DECREMENT });
