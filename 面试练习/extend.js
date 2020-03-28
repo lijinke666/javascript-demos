59a9216dc4075400810457273b8bf9de878b39f6
Jinke.Li <a1231236677287@163.com>
2020-03-28 14:16:48 +0800

    拍平数组的几种实现方式

--- 面试练习/extend.js
+++ 面试练习/extend.js
@@ -17,5 +17,21 @@ function C() {
 }
 
 C.prototype = new B()
+C.prototype.constructor = C
 const c = new C()
 c.sayName()
+
+
+function extend(A, B) {
+  function F() { }
+  F.prototype = B.prototype
+  A.prototype = new F()
+  A.prototype.constructor = A
+}
+
+extend(B, C)
+
+
+class E extends C {
+
+}
