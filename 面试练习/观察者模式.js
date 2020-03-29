1f06d2b5d0ce576dfd2166c42b21b1a4b1291e71
Jinke.Li <a1231236677287@163.com>
2020-03-29 15:01:58 +0800

    观察者模式

---
+++ 面试练习/观察者模式.js
@@ -0,0 +1,26 @@
+function createObserve() {
+  const store = {}
+  const listeners = []
+  function observe(cb) {
+    listeners.push(cb)
+  }
+
+  function dispatch() {
+    listeners.forEach((listener) => listener())
+  }
+  return {
+    observe,
+    dispatch
+  }
+}
+
+const observe = createObserve()
+
+observe.observe(() => {
+  console.log('你好');
+})
+
+observe.dispatch()
+observe.dispatch()
+observe.dispatch()
+observe.dispatch()
