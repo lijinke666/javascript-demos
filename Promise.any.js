5b5bc7140cd77dddd010135b43debb99ec69eefc
lijinke666 <lijinke666@users.noreply.github.com>
2020-10-27 08:59:16 +0000

    deploy: bae88644edf0e55cc7506adf9f12d75b9b7e5296

---
+++ Promise.any.js
@@ -0,0 +1,6 @@
+const promise1 = Promise.resolve(100);
+const promise2 = new Promise((resolve, reject) => setTimeout(reject, 100, 'info'));
+const promise3 = new Promise((resolve, reject) => setTimeout(resolve, 200, 'name'))
+
+Promise.allSettled([promise1, promise2, promise3]).
+    then((results) => console.log(results));
